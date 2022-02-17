dde0a249a3284ecde25a3c39d2827fc98da28d95
MYPC <MYPC@DESKTOP-OC882SN>
2022-02-17 13:57:06 -0800

    Initial setup of java project

---
+++ src/com/demo/app/Test.java
@@ -0,0 +1,22 @@
+package com.demo.app;
+
+public class Test {
+	
+	public static String VersionNumber="1";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String haveMoodToPlay = "yes";
+		
+		if(haveMoodToPlay.equals("yes")) {
+			System.out.println("Lets play a game");
+		}
+		else {
+			System.out.println("No mood to play, so just sleep");
+		}
+		
+		
+	}
+
+}
