e46168147d8c5f466ad461360fb0f7dc62309857
dileep2982 <70558822+dileep2982@users.noreply.github.com>
2022-06-03 09:44:02 -0700

    Added Start sysout

--- src/com/demo/app/Test.java
+++ src/com/demo/app/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("**** The Start ****");//
 		String haveMoodToPlay = "yes";
 		
 		if(haveMoodToPlay.equals("yes")) {
