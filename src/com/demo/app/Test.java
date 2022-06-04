33d7a2d2939809530161604b54a4f7139ffae11f
dileep2982 <dileep.sattaru@gmail.com>
2022-06-04 10:35:31 -0700

    Formatted the code

--- src/com/demo/app/Test.java
+++ src/com/demo/app/Test.java
@@ -5,12 +5,8 @@ public class Test {
 	public static String VersionNumber="1";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-
-		System.out.println("**** The Start **** Start Camel");//
 		String haveMoodToPlay = "yes";
-		
 		if(haveMoodToPlay.equals("yes")) {
 			System.out.println("Lets play a game");
 			System.out.println("Another sos in if: this should be master only");
@@ -19,9 +15,6 @@ public class Test {
 			System.out.println("No mood to play, so just sleep");
 			System.out.println("Another sos..");//
 		}
-		System.out.println("**** The End ****");
-		
-		
 	}
 
 }
